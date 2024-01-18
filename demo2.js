99208bad79077308dc8bd182cbb3a5bfa4ca1535
JasonLei <jlei@activision.com>
2024-01-17 20:14:58 -0800

    completed demo2

--- demo2.js
+++ demo2.js
@@ -1,28 +1,38 @@
 class Reservation extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        
-      };
-  
-      this.handleInputChange = this.handleInputChange.bind(this);
-    }
-  
-    handleInputChange(event) {
-      
-    }
-  
-    render() {
-      return (
-        <form>
-          
-        </form>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
-  
-  ReactDOM.render(
-    <Reservation />,
-    document.getElementById('root')
-  );
-  
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = event.target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.value;
+    this.setState({ [name] : value });
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Is going:
+          <input name="isGoing" type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange}></input>
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input name="numberOfGuests" type="number" value={this.state.numberOfGuests} onChange={this.handleInputChange}></input>
+        </label>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Reservation />,
+  document.getElementById('root')
+);
